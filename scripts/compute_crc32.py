cc0a23651cace4010c34bc321b88fa5e8e25b1b0
Oded Lazar <odlazar@microsoft.com>
2020-10-20 14:59:27 +0300

    fix crc32 bug

--- scripts/compute_crc32.py
+++ scripts/compute_crc32.py
@@ -4,11 +4,10 @@ import zlib
 import textwrap
 
 with open(sys.argv[1], "rb") as f:
-    data = f.read(0xffff+1)
-    crc32 = hex(zlib.crc32(data[0x4:]) & 0xffffffff)
-    reversed_crc32 = "".join(reversed(textwrap.wrap('0' + crc32[2:], 2)))
+    data = f.read(0xffff + 1)
+    crc32 = hex(zlib.crc32(data[0x4:]) & 0xffffffff).lstrip("0x")
+    reversed_crc32 = "".join(reversed(textwrap.wrap(crc32.zfill(8), 2)))
     original_crc32 = "0x" + data[:0x4].hex()
     new_crc32 = "0x" + reversed_crc32
-    print(new_crc32)
-    print(original_crc32)
-    assert original_crc32 == new_crc32
+    print(f"new:      {new_crc32}")
+    print(f"original: {original_crc32}")

